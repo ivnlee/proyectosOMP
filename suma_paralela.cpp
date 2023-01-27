cd16945f7470f2b2956cd0c394a243779a57b089
ivnlee <a01793491@tec.mx>
2023-01-26 21:25:34 -0600

    first commit

---
+++ suma_paralela.cpp
@@ -0,0 +1,44 @@
+//#include "pch.h"
+#include <iostream>
+#include "/opt/homebrew/opt/libomp/include/omp.h"
+
+#define N 15000
+#define chunk 500
+#define mostrar 15
+
+void imprimeArreglo(float *d);
+
+int main()
+{
+    std::cout << "Sumando Arreglos en Paralelo!\n";
+    float a[N], b[N], c[N];
+    int i;
+
+    for (i = 0; i < N; i++)
+    {
+        a[i] = i * 3;
+        b[i] = (i + 2.5) * 1.4;
+    }
+    int pedazos = chunk;
+
+    #pragma omp parallel for \
+    shared(a, b, c, pedazos) private(i) \
+    schedule(static, pedazos)
+
+    for (i = 0; i < N; i++)
+        c[i] = a[i] + b[i];
+
+    std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo a: " << std::endl;
+    imprimeArreglo(a);
+    std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo b: " << std::endl;
+    imprimeArreglo(b);
+    std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo c: " << std::endl;
+    imprimeArreglo(c);
+}
+
+void imprimeArreglo(float *d)
+{
+    for (int x = 0; x < mostrar; x++)
+        std::cout << d[x] << " - ";
+    std::cout << std::endl;
+}
